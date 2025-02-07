276ca22fabae8c37718ff64c993d9b3ccb7cd1d1
Henrique Sebastião <contato@henriquesebastiao.com>
2025-02-07 14:53:33 -0400

    🎨 feat: Improving web Interface

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from functools import lru_cache
 
 from fastapi import Depends, FastAPI, Form
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from spotdl import Spotdl
 from spotdl.types.options import DownloaderOptions
 from starlette.requests import Request
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+app.mount('/static', StaticFiles(directory='static'), name='static')
+app.mount('/assets', StaticFiles(directory='assets'), name='assets')
 templates = Jinja2Templates(directory='templates')
 DOWNLOADER_OPTIONS: DownloaderOptions = {
     'output': './downloads/{artists} - {title}.{output-ext}',
@@ -28,17 +28,18 @@ def get_spotdl():
 
 
 @app.get('/', response_class=HTMLResponse)
-async def index(request: Request):
+def index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
 
 
-@app.post('/download/')
-async def download(
-    spotdlc: Spotdl = Depends(get_spotdl), url: str = Form(...)
-):
+@app.post('/download/', response_class=HTMLResponse)
+def download(spotdlc: Spotdl = Depends(get_spotdl), url: str = Form(...)):
     songs = spotdlc.search([url])
-    results = spotdlc.download_songs(songs)
+    spotdlc.download_songs(songs)
 
-    if results:
-        return {'message': 'Download concluído'}
-    return {'error': 'Erro ao baixar música'}
+    return """
+    <button type="submit" class="btn btn-lg btn-light fw-bold border-white button"><i class="fa-solid fa-down-long"></i></button>
+    <div class="alert alert-success mx-auto">
+        <strong>Download completed!</strong>
+    </div>
+    """
---
+++ static/js/spinner.js
@@ -0,0 +1,4 @@
+document.getElementById('button-download').addEventListener('click', function () {
+    this.style.display = 'none';
+    document.getElementById('spinner').style.display = 'block';
+});
