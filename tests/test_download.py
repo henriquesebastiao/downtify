337522212307c5acc7a27d48c12ecd82707e576b
Henrique Sebastião <contato@henriquesebastiao.com>
2025-03-04 12:47:44 -0400

    🧪 test: Adjusting tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,4 +1,4 @@
-import os
+
 from http import HTTPStatus
 
 
@@ -11,8 +11,3 @@ def test_download_response_is_ok(client, app_container):
     )
 
     assert response.status_code == HTTPStatus.OK
-
-    file_path = './downloads/Stephen - Crossfire.mp3'
-
-    assert os.path.exists(file_path)
-    assert os.path.getsize(file_path) > 0
