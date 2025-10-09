bc8b3b2f4727c72a2bc6ee906ff579613e61db01
Henrique Sebastião <contato@henriquesebastiao.com>
2025-10-08 22:29:11 -0400

    Improving readability of entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-exec python main.py web --host 0.0.0.0 --port "${DOWNTIFY_PORT}" --keep-alive --web-use-output-dir --keep-sessions --client-id "${CLIENT_ID}" --client-secret "${CLIENT_SECRET}"
+exec python main.py web \
+    --host 0.0.0.0 \
+    --port "${DOWNTIFY_PORT}" \
+    --keep-alive \
+    --web-use-output-dir \
+    --keep-sessions
