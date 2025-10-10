522c6ed270da86eb9c7917a4e0167b4e1590da37
lapentafd <francesco.lapenta@est.tech>
2025-10-10 12:03:01 +0100

    Added the list file view
    Signed-off-by: lapentafd <francesco.lapenta@est.tech>

--- frontend/src/views/Front.vue
+++ frontend/src/views/Front.vue
@@ -1,14 +1,18 @@
 <template>
-  <Hero />
+  <div>
+    <Navbar />
+    <Hero />
+  </div>
 </template>
 
 <script>
 import { ref, onMounted } from 'vue'
 import Hero from '/src/components/Hero.vue'
+import Navbar from '/src/components/Navbar.vue'
 import API from '../model/api'
 
 export default {
-  components: { Hero },
+  components: { Navbar, Hero },
   setup() {
     onMounted(() => {
       window.scroll(0, 0)
