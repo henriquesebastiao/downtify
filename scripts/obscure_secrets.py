135088e387858b56fc19dcfac3d2c979e2d77851
Henrique Sebastião <contato@henriquesebastiao.com>
2026-02-18 06:06:15 -0400

    core: Remove secrets of file obscure_secrets.py

--- scripts/obscure_secrets.py
+++ scripts/obscure_secrets.py
@@ -5,8 +5,8 @@ def xor_bytes(data: str, key: bytes) -> bytes:
 
 KEY = b'\x4f\x2a\x91\x3c'
 
-client_id = 'c3e98c954fbd469aa7023f8be6adcf70'
-client_secret = '205557fcb24d4b328f436bb3a3bed130'
+client_id = ''
+client_secret = ''
 
 print(xor_bytes(client_id, KEY))  # b'\x2c\x59...'
 print(xor_bytes(client_secret, KEY))  # b'\x6d\x4f...'
